8df64e7d5ed8e002bd8da45f57d110e440a2a007
Artur Losiev <losieart@cvut.cz>
2026-08-25 16:59:17 +0200

    docs: checking on contiguous stack memory allocation

---
+++ memory_basics.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+    int student_age = 21;
+    
+    int university_gpa = 3.8;
+
+    std::cout << student_age << std::endl;
+    std::cout << university_gpa << std::endl;
+
+    std::cout << "Size of student_age: " << sizeof(student_age) << " bytes" << std::endl;
+    std::cout << "Size of university_gpa: " << sizeof(university_gpa) << " bytes" << std::endl;
+
+    std::cout << "Address of student_age: " << &student_age << std::endl;
+    std::cout << "Address of university_gpa: " << &university_gpa << std::endl;
+
+    return 0;
+}
